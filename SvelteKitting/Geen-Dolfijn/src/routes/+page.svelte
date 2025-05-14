afb3e62c0559d0b66bf9e4a46833ead1558502ca
Derek Verduijn <derek.verduijn@outlook.com>
2025-05-14 17:07:01 +0200

    lil changes

--- SvelteKitting/Geen-Dolfijn/src/routes/+page.svelte
+++ SvelteKitting/Geen-Dolfijn/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import sizzle from "../Images/NotBaldCat.jpg";
   import willy from "../Images/BaldCat.jpg";
+  import okkie from "../Images/Okkie<3.jpg";
 </script>
 
 <div class="header">
@@ -13,21 +14,44 @@
       Who am I? That is a good question. I do not know exactly either, but I can
       tell you what I am not: I am Definitely Not A Dolphin.
     </p>
+
+    <p>
+      What am I doing here? I am a math nerd, not computer nerd. My friends
+      indoctrinated me into coding and I do not know whether I like it or not.
+    </p>
+
+    <p>
+      Since I am still mainly a math nerd, check out my page with my favorite
+      math thingeys!
+    </p>
   </div>
 
   <img alt="My cat!" src={sizzle} class="sizzle" />
 </div>
 
 <div class="containerIntro">
-  <img alt="My friends cat!" src={willy} class="sizzle" />
+  <img alt="My other cat!" src={okkie} class="sizzle" />
 
   <div class="intro">
-    <p>More abt me here</p>
+    <p class="nob">I "know" a few programming languages:</p>
+
+    <ul class="not">
+      <li>Python (never again)</li>
+      <li>Javascript / TypeScript</li>
+      <li>Svelte (yes I'm counting that, best framework btw)</li>
+      <li>C++</li>
+    </ul>
+
+    <p>
+      Also I am still learning so if you see code that just completely sucks: Do
+      now worry, I am aware of it. Just leave an issue and I'll look into it. I
+      am too busy skateboarding on the learning curve.
+    </p>
   </div>
 </div>
 
 <div class="containerButtons">
-  <button class="button1"> Projects </button>
-  <button class="button1"> Desmos Gallery </button>
-  <button class="button1"> Its a secret </button>
+  <div class="button1"><p>Projects</p></div>
+  <div class="button1"><p>Desmos Gallery</p></div>
+  <div class="button1"><p>It's a secret</p></div>
 </div>
