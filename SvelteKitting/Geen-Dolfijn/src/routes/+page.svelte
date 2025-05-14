5924518381ae4f1a77b8d08db8f22ea187c3be52
Derek Verduijn <derek.verduijn@outlook.com>
2025-05-14 15:01:40 +0200

    Beginning made
    This is going well

---
+++ SvelteKitting/Geen-Dolfijn/src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import sizzle from "../Images/NotBaldCat.jpg";
+  import willy from "../Images/BaldCat.jpg";
+</script>
+
+<div class="header">
+  <h1>Ik ben een titel</h1>
+</div>
+
+<div class="containerIntro">
+  <div class="intro">
+    <p>
+      Who am I? That is a good question. I do not know exactly either, but I can
+      tell you what I am not: I am Definitely Not A Dolphin.
+    </p>
+  </div>
+
+  <img alt="My cat!" src={sizzle} class="sizzle" />
+</div>
+
+<div class="containerIntro">
+  <img alt="My friends cat!" src={willy} class="sizzle" />
+
+  <div class="intro">
+    <p>More abt me here</p>
+  </div>
+</div>
+
+<div class="containerButtons">
+  <button class="button1"> Projects </button>
+  <button class="button1"> Desmos Gallery </button>
+  <button class="button1"> Its a secret </button>
+</div>
---
+++ SvelteKitting/Geen-Dolfijn/src/routes/projects/+page.svelte
@@ -0,0 +1,112 @@
+<div class="header">
+  <h1 class="not nob">My Projects</h1>
+</div>
+
+<div class="containerProject">
+  <div class="projectText">
+    <h2 class="nob not">Finnish Language Trainer</h2>
+    <p class="not">
+      I learn Finnish, and I also like C++ (counter arguments will not be
+      listened to), so I combined the two to make the Finnish Language Trainer.
+    </p>
+    <p class="nob not">
+      Some things that I wish to include / add in the future are:
+    </p>
+    <ul class="nob not">
+      <li>
+        Colors, since everything happens in the terminal, colors can help
+        organising all the text, and it looks aesthetically pleasing.
+      </li>
+      <li>More grammar explainations.</li>
+      <li>
+        A way to practise numbers up to a million, but without having to type
+        the first million numbers out in a file.
+      </li>
+    </ul>
+  </div>
+
+  <div class="githubWidget">
+    <h3 class="nob not">
+      Definitely-Not-A-Dolphin /<br />
+      <a
+        href="https://github.com/Definitely-Not-A-Dolphin/Finnish-Idiom-Trainer"
+        id="github"
+        >Finnish-Language-Trainer
+      </a>
+    </h3>
+
+    <span class="nob not">
+      <p>
+        Languages: <br />
+      </p>
+      <ul>
+        <li>C++ 100%</li>
+      </ul>
+      <p>Stars: 3</p>
+    </span>
+  </div>
+</div>
+
+<div class="containerProject">
+  <div class="githubWidget">
+    <h3 class="nob not">
+      Definitely-Not-A-Dolphin /<br />
+      <a
+        href="https://github.com/Definitely-Not-A-Dolphin/Geen-Dolfijn"
+        id="github"
+        >Geen-Dolfijn
+      </a>
+    </h3>
+
+    <span class="nob not">
+      <p>
+        Languages: <br />
+      </p>
+      <ul>
+        <li>C++ 100%</li>
+      </ul>
+      <p>Stars: 3</p>
+    </span>
+  </div>
+
+  <div class="projectText">
+    <h2 class="nob not">This website</h2>
+    <p class="nob not">
+      This site is made with SvelteKit, TypeScript and all the other usual
+      stuff. My friend Jsw helped me with the setup, and will host this site on
+      his server soon.
+    </p>
+
+    <p class="nob not">
+      Some things that I wish to include / add in the future are:
+    </p>
+    <ul class="nob not">
+      <li>
+        A GitHub API, so that the data diplayed on the left here doesn't have to be entered manually.
+      </li>
+      <li>An animation on the background</li>
+      <li>
+        More banner-gifs on the frends-tab, although my friends won't make one. :(
+      </li>
+    </ul>
+  </div>
+</div>
+<!-- <div class="item containerTextImage">
+    <div class="main">
+      <h2 class="nob not" id="komputer">Finnish Language Trainer</h2>
+      <p class="not">
+        I wanted to put a webversion of this project on my site, so I got into
+        TypeScript and Svelte and I am currently making one. It is hard.
+      </p>
+      <p class="nob">
+        The things that still need to be implemented are: <br />
+        Making the practising stop after the desired amount of words, <br />
+        Adding more settings and actually making them do stuff.
+      </p>
+      <h2>Updates:</h2>
+      <p>
+        18/03/2025: Development is going good. It is really complicated but the
+        start is there.
+      </p>
+    </div>
+  </div>-->
