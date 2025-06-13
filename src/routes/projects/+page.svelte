0c23761e92ae52e53b94e16c59e52bcf653767fb
Derek Verduijn <derek.verduijn@outlook.com>
2025-06-13 17:09:59 +0200

    Push voor jurn

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { getData, getIndexFromID } from "$lib/getGitHubData.ts";
   import generalData from "$lib/generalData.json" with { type: "json" };
+  import { SimpleContextFetchingStrategy } from "discord.js";
 
   const getColor = (language: string): string => {
     for (const looper of Object.entries(generalData.languageColors)) {
@@ -12,6 +13,13 @@
     }
     return "white";
   };
+
+  const flexDirector = (counter: number): string => {
+    if (counter % 2 === 0) {
+      return "row";
+    }
+    return "row-reverse";
+  };
 </script>
 
 <div class="header">
@@ -19,6 +27,47 @@
 </div>
 
 <!-- Planning on making this an each loop -->
+
+{#snippet githubWidget(projects: any, repoId: number)}
+  {@const SNIPPET_PROJECT = projects[getIndexFromID(repoId, projects)]}
+  <h3 class="not">
+    Definitely-Not-A-Dolphin/<br />
+    <a href={SNIPPET_PROJECT.url}>
+      {SNIPPET_PROJECT.name}
+    </a>
+  </h3>
+
+  <p class="nob not">Languages</p>
+  <ul class="not">
+    {#if projects.length > 0}
+      {#each Object.entries(SNIPPET_PROJECT.languages) as [key, value]}
+        <li>
+          {value}% <span style="color: {getColor(key)}">{key}</span>
+        </li>
+      {/each}
+    {/if}
+  </ul>
+
+  <p>Stars: {SNIPPET_PROJECT.stargazers_count}</p>
+
+  <div
+    style="
+      display: flex;
+      border-radius: 5px;
+      overflow: hidden;
+    "
+  >
+    {#each Object.entries(SNIPPET_PROJECT.languages) as [key, value]}
+      <div
+        style="
+            width: {2.304 * Number(value)}px;
+            height: 12px;
+            background-color: {getColor(key)}
+        "
+      ></div>
+    {/each}
+  </div>
+{/snippet}
 
 <div class="containerStandard">
   <div class="mainStandard">
@@ -48,47 +97,7 @@
       <p>Waiting for project data...</p>
     {:then projects}
       <!-- The ID for this repo is 898363939 -->
-      {@const FIN_LANG_TRAIN_PROJECT =
-        projects[getIndexFromID(898363939, projects)]}
-      <h3 class="not">
-        Definitely-Not-A-Dolphin/<br />
-        <a href={FIN_LANG_TRAIN_PROJECT.url}>
-          {FIN_LANG_TRAIN_PROJECT.name}
-        </a>
-      </h3>
-
-      <p class="nob not">Languages:</p>
-      <ul class="not">
-        {#if projects.length > 0}
-          {#each Object.entries(FIN_LANG_TRAIN_PROJECT.languages) as [key, value]}
-            <li>
-              {value}% <span style="color: {getColor(key)}">{key}</span>
-            </li>
-          {/each}
-        {/if}
-      </ul>
-
-      <p>
-        Stars: {FIN_LANG_TRAIN_PROJECT.stargazers_count}
-      </p>
-
-      <div
-        style="
-          display: flex;
-          border-radius: 5px;
-          overflow: hidden;
-        "
-      >
-        {#each Object.entries(FIN_LANG_TRAIN_PROJECT.languages) as [key, value]}
-          <div
-            style="
-                width: {2.304 * Number(value)}px;
-                height: 12px;
-                background-color: {getColor(key)}
-            "
-          ></div>
-        {/each}
-      </div>
+      {@render githubWidget(projects, 898363939)}
     {:catch error}
       Something went wrong while fetching data; error: "{error}"
     {/await}
@@ -101,58 +110,7 @@
       <p>Waiting for project data...</p>
     {:then projects}
       <!-- The ID for this repo is 915271815 -->
-      {@const GEEN_DOLFIJN_PROJECT =
-        projects[getIndexFromID(915271815, projects)]}
-      <h3 class="not">
-        Definitely-Not-A-Dolphin /<br />
-        <a href={GEEN_DOLFIJN_PROJECT.url}>
-          {GEEN_DOLFIJN_PROJECT.name}
-        </a>
-      </h3>
-
-      <p class="nob not">Languages</p>
-      <ul class="not">
-        {#if projects.length > 0}
-          {#each Object.entries(GEEN_DOLFIJN_PROJECT.languages) as [key, value]}
-            <li>
-              {value}% <span style="color: {getColor(key)}">{key}</span>
-            </li>
-          {/each}
-        {/if}
-      </ul>
-
-      <p>
-        <svg
-          height="16"
-          viewBox="0 0 16 16"
-          version="1.1"
-          width="16"
-          style="fill: #e3b341"
-        >
-          <path
-            d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"
-          ></path>
-        </svg>
-        {GEEN_DOLFIJN_PROJECT.stargazers_count}
-      </p>
-
-      <div
-        style="
-          display: flex;
-          border-radius: 5px;
-          overflow: hidden;
-        "
-      >
-        {#each Object.entries(GEEN_DOLFIJN_PROJECT.languages) as [key, value]}
-          <div
-            style="
-                width: {2.304 * Number(value)}px;
-                height: 12px;
-                background-color: {getColor(key)}
-            "
-          ></div>
-        {/each}
-      </div>
+      {@render githubWidget(projects, 915271815)}
     {:catch error}
       Something went wrong while fetching data; error: "{error}"
     {/await}
@@ -166,17 +124,12 @@
       his server soon.
     </p>
 
-    <p class="nob not">
+    <p class="nob">
       Some things that I wish to include / add in the future are:
     </p>
     <ul class="nob not">
-      <li>
-        <s>
-          A GitHub API, so that the data displayed on the left here doesn't have
-          to be entered manually.
-        </s>
-      </li>
-      <li>An animation on the background</li>
+      <li>A way to vertically display the language bar in the widget</li>
+      <li>A better design on the background</li>
       <li>
         More banner-gifs on the frends-tab, although my friends won't make one.
         :(
@@ -192,8 +145,9 @@
       One day I woke up and had the urge to make a discord bot. I do not know
       where it came from, but here it is anyway! It can translate to morse code
       and send memes based on a number. I plan on making it run 24/7 once I get
-      a raspberry pi!
+      a raspberry pi or a server!
     </p>
+
     <p class="nob not">
       Some things that I wish to include / add in the future are:
     </p>
@@ -201,12 +155,6 @@
       <li>More memes, but I have to wait for a server first</li>
       <li>
         A way for trusted users (me, friends) to upload their memes via discord.
-      </li>
-      <li>
-        <s>
-          A quotes command (quoting random stuff from each other is a running
-          joke in our server :D)
-        </s>
       </li>
     </ul>
   </div>
@@ -216,45 +164,7 @@
       <p>Waiting for project data...</p>
     {:then projects}
       <!-- The ID for this repo is 971248396 -->
-      {@const DC_BOT_PROJECT = projects[getIndexFromID(971248396, projects)]}
-      <h3 class="not githubTitle">
-        Definitely-Not-A-Dolphin /<br />
-        <a href={DC_BOT_PROJECT.url}>{DC_BOT_PROJECT.name}</a>
-      </h3>
-
-      <p class="nob not">Languages:</p>
-      <ul class="not">
-        {#if projects.length > 0}
-          {#each Object.entries(DC_BOT_PROJECT.languages) as [key, value]}
-            <li>
-              {value}%
-              <span style="color: {getColor(key)}">{key}</span>
-            </li>
-          {/each}
-        {/if}
-      </ul>
-
-      <p>
-        Stars: {DC_BOT_PROJECT.stargazers_count}
-      </p>
-
-      <div
-        style="
-          display: flex;
-          border-radius: 5px;
-          overflow: hidden;
-        "
-      >
-        {#each Object.entries(DC_BOT_PROJECT.languages) as [key, value]}
-          <div
-            style="
-                width: {2.304 * Number(value)}px;
-                height: 12px;
-                background-color: {getColor(key)}
-            "
-          ></div>
-        {/each}
-      </div>
+      {@render githubWidget(projects, 971248396)}
     {:catch error}
       Something went wrong while fetching data; error: "{error}"
     {/await}
